8854c8d41d778da0f06685d28fee5891ff14977b
Naimid17 <54360940+Naimid17@users.noreply.github.com>
2019-10-23 11:26:02 -0300

    obligatorio 4

--- js/cart.js
+++ js/cart.js
@@ -11,17 +11,113 @@ let ERROR_MSG = "Ha habido un error :(, verifica qué pasó.";
 
 //Función que se utiliza para actualizar los costos de publicación
 function updateTotalCosts(){
+    //document.getElementById("totalCostText").innerHTML = subtotal;//
+    let articleunitCostHTML = document.getElementById("subtotalText");
+    let comissionCostHTML = document.getElementById("comissionText");
+    let totalCostHTML = document.getElementById("totalCostText");
+
+    let unitCostToShow = MONEY_SYMBOL + subtotal;
+    let comissionToShow = Math.round((comissionPercentage * 100)) + PERCENTAGE_SYMBOL;
+    let totalCostToShow = MONEY_SYMBOL + (Math.round(articleCost * comissionPercentage * 100) / 100);
+
+    articleunitCostHTML.innerHTML = subtotalShow;
+    comissionCostHTML.innerHTML = comissionToShow;
+    totalCostHTML.innerHTML = totalCostToShow;
     
 }
+document.addEventListener("DOMContentLoaded", function(e){
+    document.getElementById("articleCountInput").addEventListener("change", function(){
+        productCount = this.value;
+        updateTotalCost();
+    });
 
+    document.getElementById("articleCostInput").addEventListener("change", function(){
+        productCost = this.value;
+        updateTotalCost();
+    });
+
+    document.getElementById("premiumradio").addEventListener("change", function(){
+        comissionPercentage = 0.15;
+        updateTotalCost();
+    });
+    
+    document.getElementById("expressradio").addEventListener("change", function(){
+        comissionPercentage = 0.07;
+        updateTotalCost();
+    });
+
+    document.getElementById("standardradio").addEventListener("change", function(){
+        comissionPercentage = 0.05;
+        updateTotalCost();
+    });
+
+
+    //Configuraciones para el elemento que sube archivos
+    var dzoptions = {
+        url:"/",
+        autoQueue: false
+    };
+    var myDropzone = new Dropzone("div#file-upload", dzoptions);    
+
+
+    //Se obtiene el formulario de publicación de producto
+    var sellForm = document.getElementById("sell-info");
+
+    //Se agrega una escucha en el evento 'submit' que será
+    //lanzado por el formulario cuando se seleccione 'Vender'.
+    sellForm.addEventListener("submit", function(e){
+
+        let articleNameInput = document.getElementById("articleName");
+        let articleCategory = document.getElementById("productCategory");
+        let articleCost = document.getElementById("articleCostInput");
+        let infoMissing = false;
+
+        //Quito las clases que marcan como inválidos
+        articleNameInput.classList.remove('is-invalid');
+        articleCategory.classList.remove('is-invalid');
+        articleCost.classList.remove('is-invalid');
+
+        //Se realizan los controles necesarios,
+        //En este caso se controla que se haya ingresado el nombre y categoría.
+        //Consulto por el nombre del producto
+        if (articleNameInput.value === "")
+        {
+            articleNameInput.classList.add('is-invalid');
+            infoMissing = true;
+        }
+        
+        //Consulto por la categoría del producto
+        if (articleCategory.value === "")
+        {
+            articleCategory.classList.add('is-invalid');
+            infoMissing = true;
+        }
+
+        //Consulto por el costo
+        if (articleCost.value <=0)
+        {
+            articleCost.classList.add('is-invalid');
+            infoMissing = true;
+        }
+        
+        if(!infoMissing)
+        {
 function updateSubtotal(){
     articleCount =  document.getElementById("articleCountInput").value;
     var subtotal = articleunitCost*articleCount;
-
+    
     document.getElementById("subtotal").innerHTML = subtotal;
+    document.getElementById("articleCostText").innerHTML = subtotal;
 
 }
 
+function hidePaymentTypeNotSelected(){
+
+}
+
+function showProducts(products){
+
+}
 function showArticles(articles){
     let htmlContentToAppend=`<form id="form1">
     <div class="divTable">
